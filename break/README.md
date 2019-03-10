e645c27ddf2a40f20166bf30e2effc9e1722bdbb
FalconRay <falconray@yahoo.com>
2019-03-10 11:44:19 +0800

    Demo break instruction.

---
+++ break/README.md
@@ -0,0 +1,4 @@
+# break demo
+Understanding break in golang.
+The key is break to outside tag from deep control instructions.
+
---
+++ break/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/falconray0704/u4go/sysCfg"
+	slog "github.com/falconray0704/u4go/sysLogger"
+	"log"
+)
+
+func initSys() (clean func()){
+	var (
+		errOnce error
+	)
+
+	sysLoggerCfg := slog.NewSysLogCfg()
+	errOnce = sysCfg.LoadFileCfgs("./sysDatas/cfgs/appCfgs.yaml", "sysLogger", &sysLoggerCfg)
+	if errOnce != nil {
+		log.Fatalf("Loading system configs fail:%s\n", errOnce.Error())
+	}
+	_, _, err := slog.Init(sysLoggerCfg)
+	if err != nil {
+		log.Fatalf("Init system logger fail: %s.\n", err.Error())
+	}
+	slog.Debug("System init finished.")
+
+	return func() {
+		slog.Sync()
+		slog.Close()
+	}
+
+}
+
+var (
+	sysClean func()
+)
+
+func init() {
+	sysClean = initSys()
+}
+
+func main() {
+	defer sysClean()
+
+LOOP1:
+	for {
+		x := 1
+		switch {
+		case x > 0:
+			slog.Info("A")
+			break LOOP1
+		case x == 1:
+			slog.Info("B")
+		default:
+			slog.Info("C")
+		}
+	}
+
+}
+
